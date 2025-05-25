44bea69ebba27a91b01992f4aaac00d2c33b3fd5
Charlotte Knight <charlotteroseknight@outlook.com>
2025-05-25 17:45:46 +0100

    is faster

--- env.sh
+++ env.sh
@@ -9,6 +9,6 @@ else
   mv bin pylhe_env
   export MAMBA_ROOT_PREFIX=${PWD}/pylhe_env
   eval "$(./pylhe_env/micromamba shell hook -s posix)"
-  micromamba env create -n pylhe -c conda-forge pylhe uproot tqdm -y
+  micromamba env create -n pylhe -c conda-forge pylhe uproot -y
   micromamba activate pylhe
 fi
--- nanoToLHE.py
+++ nanoToLHE.py
@@ -1,10 +1,8 @@
 import uproot
 import pylhe
 import sys
-import vector
 import awkward as ak
-import tqdm
-vector.register_awkward()
+import numpy as np
 
 assert len(sys.argv) > 2, "Please provide input and output file names as arguments."
 print("Reading LHE file from", sys.argv[1])
@@ -16,48 +14,63 @@ else:
   start, stop = 0, None
 print("Processing events", start, "to", stop)
 
+print("Loading events from file")
 f = uproot.open(sys.argv[1])
-
 fields = ["pt", "eta", "phi", "mass", "pdgId", "status", "spin"]
-lhepart = f["Events"].arrays([f"LHEPart_{field}" for field in fields], library="ak", entry_start=start, entry_stop=stop)
-events = ak.zip({field: lhepart[f"LHEPart_{field}"] for field in fields}, with_name="Momentum4D")
+events = f["Events"].arrays([f"LHEPart_{field}" for field in fields], library="ak", entry_start=start, entry_stop=stop)
+nLHEPart = f["Events/nLHEPart"].array(library="ak", entry_start=start, entry_stop=stop)
 
-lheevents = []
+lheevents = np.empty(len(events), dtype=object)
+idx = np.arange(len(events))
 
-for parts in tqdm.tqdm(events):
-  lheeventinfo = pylhe.LHEEventInfo(
-    nparticles = len(parts),
-    pid = 0,
-    weight = 1.0, # may need to be changed
-    scale = 100.0, # may need to be changed
-    aqed = 1/137, # may need to be changed
-    aqcd = 0.1181 # may need to be changed
-  )
-  
-  lheparticles = []
-  for part in parts:
-    lheparticle = pylhe.LHEParticle(
-      id = part.pdgId,
-      status = part.status,
-      spin = part.spin,
-      px = part.px,
-      py = part.py,
-      pz = part.pz,
-      e = part.e,
-      m = part.m,
-      mother1 = 0, # can be extracted from genpart if needed
-      mother2 = 0, # can be extracted from genpart if needed
-      color1 = 0,
-      color2 = 0,
-      lifetime = 0
+print("Creating LHE events")
+for n in np.unique(nLHEPart):
+  sevents = events[nLHEPart == n]
+  sidx = idx[nLHEPart == n]
+
+  px = sevents["LHEPart_pt"].to_numpy() * np.cos(sevents["LHEPart_phi"].to_numpy())
+  py = sevents["LHEPart_pt"].to_numpy() * np.sin(sevents["LHEPart_phi"].to_numpy())
+  pz = sevents["LHEPart_pt"].to_numpy() * np.sinh(sevents["LHEPart_eta"].to_numpy())
+  e = np.sqrt(sevents["LHEPart_mass"].to_numpy()**2 + px**2 + py**2 + pz**2)
+  m = sevents["LHEPart_mass"].to_numpy()
+  pdgId = sevents["LHEPart_pdgId"].to_numpy()
+  status = sevents["LHEPart_status"].to_numpy()
+  spin = sevents["LHEPart_spin"].to_numpy()
+
+  for i in range(len(sevents)):
+    lheeventinfo = pylhe.LHEEventInfo(
+      nparticles = n,
+      pid = 0, # may need to be changed
+      weight = 1.0, # may need to be changed
+      scale = 100.0, # may need to be changed
+      aqed = 1/137, # may need to be changed
+      aqcd = 0.1181 # may need to be changed
     )
-    lheparticles.append(lheparticle)
 
-  lheevent = pylhe.LHEEvent(
-    eventinfo = lheeventinfo,
-    particles = lheparticles,
-  )
-  lheevents.append(lheevent)
+    lheparticles = []
+    for j in range(n):
+      lheparticle = pylhe.LHEParticle(
+        id = pdgId[i][j],
+        status = status[i][j],
+        spin = spin[i][j],
+        px = px[i][j],
+        py = py[i][j],
+        pz = pz[i][j],
+        e = e[i][j],
+        m = m[i][j],
+        mother1 = 0, # can be extracted from genpart if needed
+        mother2 = 0, # can be extracted from genpart if needed
+        color1 = 0,
+        color2 = 0,
+        lifetime = 0
+      )
+      lheparticles.append(lheparticle)
+
+    lheevent = pylhe.LHEEvent(
+      eventinfo = lheeventinfo,
+      particles = lheparticles,
+    )
+    lheevents[sidx[i]] = lheevent
 
 # no idea if all of this matters at all
 lheinit = pylhe.LHEInit(
@@ -85,4 +98,5 @@ lheinit = pylhe.LHEInit(
     LHEVersion=3,
 )
 
+print("Writing lhe")
 pylhe.write_lhe_file(lheinit, lheevents, sys.argv[2])
